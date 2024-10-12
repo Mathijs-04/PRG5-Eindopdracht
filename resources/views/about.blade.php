0f8bc9deb54fe86667bb0fc4c0a925e699df12b5
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-12 12:34:07 +0200

    Views edited

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,4 @@
 <h1>About</h1>
 <p>This is the About page</p>
+<a href="http://127.0.0.1:8000/home">Home</a>
+K
--- resources/views/models.blade.php
+++ resources/views/models.blade.php
@@ -1,2 +1,3 @@
 <h1>Models</h1>
 <p>This is the Models page</p>
+<a href="http://127.0.0.1:8000/home">Home</a>
