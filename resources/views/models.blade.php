195d326da3d14d4c4726e04d2baf926883dfe382
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-14 16:20:08 +0200

    Grid added to models view

--- resources/views/models.blade.php
+++ resources/views/models.blade.php
@@ -1,3 +1,18 @@
 <x-layout>
-    <h1 class="pb-[70vh]">Welcome to the Models page</h1>
+    <h1 class="pb-[5vh]">Welcome to the Models page</h1>
 </x-layout>
+<div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 pb-[5vh]">
+    <div class="h-32 rounded-lg bg-red-950"></div>
+    <div class="h-32 rounded-lg bg-red-950"></div>
+    <div class="h-32 rounded-lg bg-red-950"></div>
+</div>
+<div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 pb-[5vh]">
+    <div class="h-32 rounded-lg bg-red-950"></div>
+    <div class="h-32 rounded-lg bg-red-950"></div>
+    <div class="h-32 rounded-lg bg-red-950"></div>
+</div>
+<div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 pb-[5vh]">
+    <div class="h-32 rounded-lg bg-red-950"></div>
+    <div class="h-32 rounded-lg bg-red-950"></div>
+    <div class="h-32 rounded-lg bg-red-950"></div>
+</div>
