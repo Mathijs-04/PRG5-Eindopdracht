df0fee43c44be250d8c4fe9295253084916b259c
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-15 11:39:22 +0200

    Edited posts loop

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,14 @@
 <x-layout>
     <h1 class="mb-[5vh]">Welcome to the Posts page</h1>
-    <ul>
-        @foreach($posts as $post)
-            <li>{{ $post ->title }}</li>
-        @endforeach
-    </ul>
+    @foreach($posts as $post)
+        <section>
+            <br>
+            {{ $post ->title }}
+            <br>
+            {{ $post ->description }}
+            <br>
+            {{ $post ->image_url }}
+            <br>
+        </section>
+    @endforeach
 </x-layout>
