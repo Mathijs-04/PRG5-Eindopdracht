7b9ace1ac3d81d1d2dbea88a574a3e3564770880
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-15 11:35:07 +0200

    Added database posts to posts view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts');
+        $posts = Post::all();
+        return view('posts', compact('posts'));
+    }
+
+    public function show($id) {
+        $post = Post::find($id);
+        return view('show', compact('id'));
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,8 @@
 <x-layout>
-    <h1 class="pb-[5vh]">Welcome to the Posts page</h1>
+    <h1 class="mb-[5vh]">Welcome to the Posts page</h1>
+    <ul>
+        @foreach($posts as $post)
+            <li>{{ $post ->title }}</li>
+        @endforeach
+    </ul>
 </x-layout>
-<div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 pb-[5vh]">
-    <div class="h-32 rounded-lg bg-red-950"></div>
-    <div class="h-32 rounded-lg bg-red-950"></div>
-    <div class="h-32 rounded-lg bg-red-950"></div>
-</div>
-<div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 pb-[5vh]">
-    <div class="h-32 rounded-lg bg-red-950"></div>
-    <div class="h-32 rounded-lg bg-red-950"></div>
-    <div class="h-32 rounded-lg bg-red-950"></div>
-</div>
-<div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 pb-[5vh]">
-    <div class="h-32 rounded-lg bg-red-950"></div>
-    <div class="h-32 rounded-lg bg-red-950"></div>
-    <div class="h-32 rounded-lg bg-red-950"></div>
-</div>
