8a8c3813300da2097f0fa362dcb3947a134a8d82
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-16 16:29:13 +0200

    Added one-to-many relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
