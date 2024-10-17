b56abc2e6ef5a1f16519bafb002bd64733d7d63a
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-17 10:45:45 +0200

    Added Logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,10 +10,10 @@
 
 <nav class="bg-slate-700 border-gray-200 dark:bg-slate-700">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"/>
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
+        <div class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="images/logo.webp" class="h-20" alt="Logo"/>
+            <span class="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Mighty Models</span>
+        </div>
         <button data-collapse-toggle="navbar-default" type="button"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-default" aria-expanded="false">
@@ -35,7 +35,7 @@
                     <a href="/about" class="{{ Request::is('about') ? 'text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' }}">About</a>
                 </li>
                 <li>
-                    <a href="/check" class="{{ Request::is('check') ? 'text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' }}">Check</a>
+                    <a href="/posts/create" class="{{ Request::is('posts.create') ? 'text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' }}">Create</a>
                 </li>
                 @if (Route::has('login'))
                     @auth
