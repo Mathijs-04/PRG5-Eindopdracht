4cc2884b109d920f9d4131b1eeecb8626384afe6
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-17 09:51:41 +0200

    Edited tailwind styling

--- resources/views/components/test.blade.php
+++ resources/views/components/test.blade.php
@@ -9,7 +9,9 @@
     @vite('resources/css/app.css')
 </head>
 
-<nav class="bg-slate-700 border-gray-200 dark:bg-slate-700">
+<body>
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"/>
@@ -25,7 +27,7 @@
             </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-slate-700 dark:border-gray-700">
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                     <a href="/"
                        class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
@@ -79,15 +81,9 @@
     </div>
 </nav>
 
-<body class="bg-gray-900">
+{{$slot}}
 
-<section class="px-8 md:px-64">
-    {{$slot}}
-</section>
-
-</body>
-
-<footer class="footer footer-center  w-full p-4 bg-slate-700 text-white">
+<footer class="footer footer-center  w-full p-4 bg-gray-300 text-gray-800">
     <div class="text-center">
         <p class="font-bold">
             Copyright © 2024 - Mighty Models
@@ -95,4 +91,5 @@
     </div>
 </footer>
 
+</body>
 </html>
