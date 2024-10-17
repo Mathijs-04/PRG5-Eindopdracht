92cf256e594245bc93b520937681d474500f9231
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-17 11:22:33 +0200

    Edited styling

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
-    <h1 class="text-4xl text-white font-sans my-4">Welcome to the Posts page</h1>
-    <div class="grid-container">
+    <div class="grid-container pt-20">
         @foreach($posts as $post)
             <div class="max-w-sm bg-black-600 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
@@ -10,7 +9,7 @@
                     <a href="#">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $post->description }}</p>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $post->tag }}</p>
                     <a href="{{ route('show', ['id' => $post->id]) }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Details
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -21,7 +20,7 @@
             </div>
         @endforeach
     </div>
-    <div class="pb-[38.5vh]"></div>
+    <div class="pb-[20vh]"></div>
 </x-layout>
 
 <style>
