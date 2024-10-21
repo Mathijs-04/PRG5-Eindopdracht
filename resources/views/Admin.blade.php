d3124e151a3ca37e3180ee6972477f4da84aa8f1
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-10-21 12:12:41 +0200

    Secured admin page with redirect

--- resources/views/Admin.blade.php
+++ resources/views/Admin.blade.php
@@ -2,7 +2,8 @@
     @if (auth()->user()->is_admin)
         <h1 class="text-4xl text-white font-sans my-4">Welcome Admin</h1>
     @else
-        <h1 class="text-4xl text-white font-sans my-4">You are not the Admin, get out of here!</h1>
-        <a href="/" class="text-2xl text-blue-500 hover:text-blue-700 underline">Home</a>
+        <script>
+            window.location.href = " {{ route('home') }}"
+        </script>
     @endif
 </x-layout>
